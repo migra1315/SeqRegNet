c224287ebee3281a66785e350ed0829bf90709d3
migra1315 <1791575694@qq.com>
2022-04-20 09:41:46 +0800

    Full version of Fucr, Ucr and Cr;config with yaml file;all train result info was saved in yaml file

---
+++ run.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=0 python ../train_one_shot.py -case 1 --remote --config ./config/config_ucr.yaml
+CUDA_VISIBLE_DEVICES=0 python ../train_one_shot.py -case 8 --remote --config ./config/config_ucr.yaml
+
+
